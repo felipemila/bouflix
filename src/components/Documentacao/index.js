82f506e08c19e9f9ebf902c1e79607abe3d345f4
Felipe Milanez <fmilanez@beonup.com.br>
2020-07-28 16:29:30 -0300

    teste

--- src/components/Documentacao/index.js
+++ src/components/Documentacao/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Documentacao } from './styles';
 
-const enviar = async () => {
-  alert('Cadastro realizado. Obrigado!') 
-}
 
 function Capturar() {
   return (
@@ -11,7 +8,8 @@ function Capturar() {
       <h1>DOCUMENTAÇÃO</h1>
       <p>Guias para iniciantes</p>
       <a href="https://desafio-bou.s3.amazonaws.com/AWS+S3+-+Guia+Iniciante.pdf">AWS S3 Básico</a>
-      <a href="https://beonupcombr-my.sharepoint.com/:w:/g/personal/holiveira_beonup_com_br/EfItf0rVpC1Mta3m6GqVHQUBym-pHAmIDHiaEvDSEPutXg?e=Og7t8i">CSV para JSON com Glue</a> 
+      <a href="https://beonupcombr-my.sharepoint.com/:w:/g/personal/holiveira_beonup_com_br/EfItf0rVpC1Mta3m6GqVHQUBym-pHAmIDHiaEvDSEPutXg?e=Og7t8i">CSV para JSON com Glue</a>
+      <a href="https://desafio-bou.s3.amazonaws.com/Abrindo+tabela+simples+XLS+com+python.pdf">Abrir tabela XLS com Python</a>
       <p>Documentações oficiais dos serviços AWS</p>
       <a href="https://docs.aws.amazon.com/pt_br/AmazonS3/latest/dev/Introduction.html">S3</a>
       <a href="https://docs.aws.amazon.com/pt_br/glue/latest/dg/how-it-works.html">Glue</a>
